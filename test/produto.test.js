99c06e5921cc468c409af325749cb8ec9d2fda61
JohnnysMartins <johnnys.adson@gmail.com>
2018-03-19 23:10:15 -0300

    criando mais teste de produtos.

--- test/produto.test.js
+++ test/produto.test.js
@@ -1,13 +1,25 @@
 var express = require('../config/express')();
-var http = require('supertest')(express);
+var request = require('supertest')(express);
 
 describe('#ProdutosController', function () {
     it('listagem json', function (done) {
-        http.get('/produtos')
+        request.get('/produtos')
             .set('accept', 'application/json')
             .expect('content-type', /json/)
             .expect(200, done);
 
+    });
+
+    it('#Cadastrar um novo protudo com dados invalidos', (done) => {
+        request.post('/produtos')
+            .send({titulo : "", descricao: "novo livro"})
+            .expect(400, done);
+    });
+
+        it('#Cadastrar um novo protudo com dados validos', (done) => {
+        request.post('/produtos')
+            .send({titulo : "titulo",preco: 20.21, descricao: "novo livro"})
+            .expect(302, done);
     });
 });
 
